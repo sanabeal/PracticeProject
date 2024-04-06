a2cc30073cd305797fade1f06ab4f61266876bb9
Sanabeal Kabir <sanabealkabir@gmail.com>
2024-04-06 14:51:05 +0600

    MauiFirstApps

---
+++ MauiApp1/MauiApp1/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public bool _isBusy;
+        [ObservableProperty]
+        public string _title;
+    }
+}
---
+++ MauiApp1/MauiApp1/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModels
+{
+    public partial class LoginPageViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private string _userName;
+        [ObservableProperty]
+        private string _password;
+
+        [ICommand]
+        public async void Login() 
+        {
+            string vds = "text";
+        }
+    }
+}
